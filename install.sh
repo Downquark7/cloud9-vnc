8c43025b4adebf5abca57b006dd9bf315c95f1b8
Downquark7 <mail2emn@gmail.com>
2016-10-24 13:20:24 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/ubuntu
-
+sudo apt-get update
 sudo apt-get install supervisor xvfb lxde x11vnc
 git clone git://github.com/kanaka/noVNC
 mv /home/ubuntu/workspace/cloud9-vnc/run.sh /home/ubuntu/workspace/
