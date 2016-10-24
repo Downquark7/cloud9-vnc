f52fc718df28e56af6f937095f422865e2fbe9af
Downquark7 <mail2emn@gmail.com>
2016-10-24 12:31:15 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 
 cd /home/ubuntu
 
-sudo apt-get install supervisor xvfb fluxbox x11vnc
+sudo apt-get install supervisor xvfb lxde x11vnc
 git clone git://github.com/kanaka/noVNC
+mv /home/ubuntu/workspace/cloud9-vnc/run.sh /home/ubuntu/workspace/
